6f21df09727ded70ec924b8e96b0e0763c39b429
MinhNguyenk3 <minhnguyenk3@gmail.com>
2023-02-16 10:35:26 +0700

    first commit

---
+++ NguyenVanMinh_Project/src/nguyenvanminh_project/NguyenVanMinh_App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nguyenvanminh_project;
+
+/**
+ *
+ * @author dell
+ */
+public class NguyenVanMinh_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nguyen Van Minh hoc Java");
+    }
+    
+}
